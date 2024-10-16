4be662b34d872b1f20aee7db90bc0526d8d88890
Yash <yash.verma@instalanding.in>
2024-10-16 21:25:43 +0530

    fix fetch api

--- lib/backendFunctions.ts
+++ lib/backendFunctions.ts
@@ -5,7 +5,7 @@ export const parseIp = (req: any) =>
 
 export const fetchApi = async (userIP:string) => {
     try {
-        const response = await axios.get(`https://ipgeolocation.abstractapi.com/v1/?api_key==21d8f763805b42139f1cb83fb37bbe1b&ip_address=${userIP}`);
+        const response = await axios.get(`https://ipgeolocation.abstractapi.com/v1/?api_key=21d8f763805b42139f1cb83fb37bbe1b&ip_address=${userIP}`);
         return response.data;
     } catch (err) {
         console.error(err);
