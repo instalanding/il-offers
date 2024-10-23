5dacb7e4c918a0faf92cc2bcd4dc836472bf723a
Yash <yash.verma@instalanding.in>
2024-10-23 19:47:33 +0530

    add new changes

--- app/api/coupon/route.ts
+++ app/api/coupon/route.ts
@@ -39,6 +39,7 @@ export const GET = async (req: Request) => {
         // Include the store logo in the response
         campaign.store_logo = advertiser.store_logo;
         campaign.store_url = advertiser.shop_url;
+        campaign.domains = advertiser.domains;
         if(advertiser.checkout){
             campaign.checkout = advertiser.checkout;
         }
