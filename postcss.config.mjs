548912fc5c233696031a6c0de35add07a7c27a40
Lalit8525 <lalit.kumar@instalanding.in>
2024-10-24 14:03:21 +0530

    Worked on LCP

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,16 +2,6 @@
 const config = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
-    '@fullhuman/postcss-purgecss': {
-      content: ['./pages/**/*.js', './components/**/*.js'],
-      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-      safelist: {
-        standard: ['html', 'body'],
-        deep: [],
-        greedy: []
-      }
-    }
   },
 };
 
