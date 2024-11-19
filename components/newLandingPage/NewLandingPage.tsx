79a79d6032c32693de3c6a8e78088ad3d9447871
Chhavi <chhavi.gupta@instalanding.in>
2024-11-19 11:42:54 +0530

    fetching sorted variants based on offer price

--- components/newLandingPage/NewLandingPage.tsx
+++ components/newLandingPage/NewLandingPage.tsx
@@ -80,12 +80,6 @@ const NewLandingPage = ({
     fetchData();
   }, [currentVariantId, schema.product_handle]); // Dependency on currentVariantId
 
-  // Sort campaigns by offer price
-  const sortedCampaigns = currentSchema.all_campaigns?.sort((a: any, b: any) => {
-    const offerPriceA = parseFloat(a.price.offerPrice.value);
-    const offerPriceB = parseFloat(b.price.offerPrice.value);
-    return offerPriceA - offerPriceB; // Ascending order
-  }) || [];
 
   return (
     <div
@@ -173,7 +167,7 @@ const NewLandingPage = ({
         )}
 
 {!offer_ids.includes(offer_id) ? <>
-  {currentSchema.all_campaigns && sortedCampaigns.length > 1 && (
+  {currentSchema.all_campaigns && currentSchema.all_campaigns.length > 1 && (
           <div className="my-3 bg-white px-4 rounded-lg">
             <h1 className="flex flex-col text-[17px] mb-2 font-semibold">Available Options
               {currentVariantId && (
@@ -186,52 +180,54 @@ const NewLandingPage = ({
               )}
             </h1>
 
-            <div className="grid grid-cols-3 gap-2">
-              {sortedCampaigns.map((product: any) => (
-                <div
-                  key={product._id}
-                  className={`cursor-pointer border-2 rounded-lg p-2 hover:shadow-[0_6px_15px_rgba(0,0,0,0.4)] ${product.variant_id === currentVariantId ? 'border-2 border-black shadow-[0_4px_10px_rgba(0,0,0,0.4)]' : ''}`}
-                  onClick={() => {
-                    setCurrentVariantId(product.variant_id); // Update selected variant
-                  }}
-                >
-                  <Link
-                    href={`/products/${currentSchema.product_handle}?variant_id=${product.variant_id}`}
+            <div className="overflow-x-auto">
+              <div className="flex space-x-2">
+                {currentSchema.all_campaigns.map((product: any) => (
+                  <div
+                    key={product._id}
+                    className={`flex-shrink-0 w-[calc(100%/2.5-1rem)] cursor-pointer border-2 rounded-lg p-2 hover:shadow-[0_6px_15px_rgba(0,0,0,0.4)] ${product.variant_id === currentVariantId ? 'border-2 border-black shadow-[0_4px_10px_rgba(0,0,0,0.4)]' : ''}`}
+                    onClick={() => {
+                      setCurrentVariantId(product.variant_id); // Update selected variant
+                    }}
                   >
-                    <Image
-                      alt={product.variant_type ? product.variant_type : "Variant"}
-                      src={product.creative.image}
-                      width={60}
-                      height={50}
-                      className="justify-self-center"
-                    />
-                    <h2 className="text-[14px] font-semibold">
-                      {truncateText(product.variant_type ? product.variant_type : product.campaign_name, 25)}
-                    </h2>
-                    <div className="flex items-center">
-                      {(parseFloat(product?.price?.offerPrice?.value) < parseFloat(product?.price?.originalPrice?.value)) ? (
-                        <div className="flex flex-col gap-1">
-                          <div className="flex justify-center items-center gap-1">
-                            <p className="text-[13px] text-gray-600 line-through">
-                              {product.price.originalPrice.prefix}{product.price.originalPrice.value}
-                            </p>
-                            <p className="text-[15px] font-semibold text-green-600">
-                              {product.price.offerPrice.prefix}{product.price.offerPrice.value}
+                    <Link
+                      href={`/products/${currentSchema.product_handle}?variant_id=${product.variant_id}`}
+                    >
+                      <Image
+                        alt={product.variant_type ? product.variant_type : "Variant"}
+                        src={product.creative.image}
+                        width={60}
+                        height={50}
+                        className="justify-self-center"
+                      />
+                      <h2 className="text-[14px] font-semibold">
+                        {truncateText(product.variant_type ? product.variant_type : product.campaign_name, 25)}
+                      </h2>
+                      <div className="flex items-center">
+                        {(parseFloat(product?.price?.offerPrice?.value) < parseFloat(product?.price?.originalPrice?.value)) ? (
+                          <div className="flex flex-col gap-1">
+                            <div className="flex justify-center items-center gap-1">
+                              <p className="text-[13px] text-gray-600 line-through">
+                                {product.price.originalPrice.prefix}{product.price.originalPrice.value}
+                              </p>
+                              <p className="text-[15px] font-semibold text-green-600">
+                                {product.price.offerPrice.prefix}{product.price.offerPrice.value}
+                              </p>
+                            </div>
+                            <p className="text-[13px] text-red-600">
+                              {calculatePercentageOff(parseFloat(product.price.originalPrice.value), parseFloat(product.price.offerPrice.value))}% off
                             </p>
                           </div>
-                          <p className="text-[13px] text-red-600">
-                            {calculatePercentageOff(parseFloat(product.price.originalPrice.value), parseFloat(product.price.offerPrice.value))}% off
+                        ) : (
+                          <p className="text-[15px] font-semibold text-green-600">
+                            {product.price.originalPrice.prefix}{product.price.originalPrice.value}
                           </p>
-                        </div>
-                      ) : (
-                        <p className="text-[15px] font-semibold text-green-600">
-                          {product.price.originalPrice.prefix}{product.price.originalPrice.value}
-                        </p>
-                      )}
-                    </div>
-                  </Link>
-                </div>
-              ))}
+                        )}
+                      </div>
+                    </Link>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         )}
