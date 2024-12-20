884d7b47b94e026a2425ff7f5fe6571e8eabcff1
Lalit Sahu <lalit.kumar@instalanding.in>
2024-12-20 11:40:10 +0530

    Some changes

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,6 +7,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 1,
+        },
+        {
+            url: "https://instalanding.shop/f1211",
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 2,
         }
     ]
 }
