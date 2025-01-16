6e38bfadb66c5dedff4edb71e6f3f9e89517bd70
yash-instalanding <yash.verma@instalanding.in>
2025-01-17 00:58:33 +0530

    add new changes

---
+++ app/dlt/[offer_id]/Redirect.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import React, { useEffect } from "react";
+import FingerprintJS from "@fingerprintjs/fingerprintjs";
+import { userAgent } from "next/server";
+import { permanentRedirect } from "next/navigation";
+import { new_backend_url } from "@/utils/constants";
+
+const Redirect = ({ data }: any) => {
+  if (!data) {
+    return <div>Deeplink not found</div>;
+  }
+
+  const getVisitorId = async () => {
+    if (typeof window === "undefined") return;
+
+    try {
+      const fp = await FingerprintJS.load();
+      const result = await fp.get();
+      console.log(result.visitorId);
+      hit(result.visitorId);
+    } catch (error) {
+      console.error("Error getting visitor identifier:", error);
+      return null;
+    }
+  };
+
+  const hit = async (fp: string) => {
+    console.log(new_backend_url);
+
+    await fetch(
+      `${new_backend_url}record-redirect/?fp=${fp}&offer_id=${data.offer_id}&advertiser=${data.advertiser}`,
+      {
+        cache: "no-store",
+      }
+    );
+  };
+
+  let redirectUrl = data.product_url;
+  let href = data.product_url;
+  const buttonType: string = data.cta_type;
+
+  if (buttonType === "amazon") {
+    redirectUrl = `${process.env.NEXT_PUBLIC_REDIRECT_SCRIPT_URL}amazon-redirect/?redirect_url=${href}&ctatype=${buttonType}`;
+  } else {
+    if (/android/i.test(userAgent.toString())) {
+      redirectUrl = `intent:${href.replace(
+        /^https?:\/\//,
+        ""
+      )}#Intent;package=com.android.chrome;scheme=https;action=android.intent.action.VIEW;end;`;
+    } else if (
+      /iPad|iPhone|iPod/.test(userAgent.toString()) &&
+      !/windows/i.test(userAgent.toString())
+    ) {
+      redirectUrl = href.startsWith("http") ? href : `https://${href}`;
+    }
+  }
+
+  useEffect(() => {
+    getVisitorId().then(permanentRedirect(redirectUrl));
+  }, []);
+
+  return <>redirecting...</>;
+};
+
+export default Redirect;
--- app/dlt/[offer_id]/page.tsx
+++ app/dlt/[offer_id]/page.tsx
@@ -1,7 +1,7 @@
 "use server";
 import React from "react";
-import { userAgent } from "next/server";
-import { permanentRedirect } from "next/navigation";
+import Redirect from "./Redirect";
+import { new_backend_url } from "@/utils/constants";
 
 interface PageProps {
   params: { offer_id: string };
@@ -10,9 +10,9 @@ interface PageProps {
 
 const getDeeplink = async (offer_id: string) => {
   try {
-    console.log(`${process.env.API_URL}deeplink/${offer_id}`);
+    console.log(`${new_backend_url}deeplink/${offer_id}`);
 
-    const response = await fetch(`${process.env.API_URL}deeplink/${offer_id}`, {
+    const response = await fetch(`${new_backend_url}deeplink/${offer_id}`, {
       cache: "no-store",
     });
 
@@ -30,30 +30,13 @@ const getDeeplink = async (offer_id: string) => {
 const Page: React.FC<PageProps> = async ({ params, searchParams }) => {
   const { offer_id } = params;
 
+  // data fetched here
+
   const data = await getDeeplink(offer_id);
 
-  console.log(data);
+  // redirect happens here
 
-  let redirectUrl = data.product_url;
-  let href = data.product_url;
-  const buttonType: string = data.cta_type;
-
-  if (buttonType === "amazon") {
-    redirectUrl = `${process.env.REDIRECT_SCRIPT_URL}amazon-redirect/?redirect_url=${href}&ctatype=${buttonType}`;
-  } else {
-    if (/android/i.test(userAgent.toString())) {
-      redirectUrl = `intent:${href.replace(
-        /^https?:\/\//,
-        ""
-      )}#Intent;package=com.android.chrome;scheme=https;action=android.intent.action.VIEW;end;`;
-    } else if (
-      /iPad|iPhone|iPod/.test(userAgent.toString()) &&
-      !/windows/i.test(userAgent.toString())
-    ) {
-      redirectUrl = href.startsWith("http") ? href : `https://${href}`;
-    }
-  }
-  permanentRedirect(redirectUrl);
+  return <Redirect data={data} />;
 };
 
 export default Page;
