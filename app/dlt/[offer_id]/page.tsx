2f5d073e191e79d68ea46926ba36ad1b9c78364b
Lalit Sahu <lalit.kumar@instalanding.in>
2025-02-05 14:25:36 +0530

    Generate metadata

--- app/dlt/[offer_id]/page.tsx
+++ app/dlt/[offer_id]/page.tsx
@@ -4,7 +4,7 @@ import Redirect from "./Redirect";
 import { new_backend_url } from "@/utils/constants";
 import { userAgent } from "next/server";
 import { permanentRedirect } from "next/navigation";
-
+import { Metadata, ResolvingMetadata } from "next";
 interface PageProps {
   params: { offer_id: string };
   searchParams: { mode: string; user_ip?: any };
@@ -48,3 +48,53 @@ const Page: React.FC<PageProps> = async ({ params, searchParams }) => {
 };
 
 export default Page;
+
+
+export async function generateMetadata(
+  { params, searchParams }: { params: { offer_id: string; }; searchParams: any },
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const { offer_id } = params;
+  const data = await getDeeplink(offer_id);
+
+  const title = data?.meta_title || "Instalanding Offers";
+  const description = data?.meta_description || "Explore exclusive offers with Instalanding.";
+  const imageUrl = data?.meta_description?.image?.url || "/default-meta-image.jpg";
+
+  return {
+    title,
+    description,
+    icons: data?.advertiser?.store_logo?.url
+      ? [{ rel: "icon", url: data.advertiser.store_logo.url.toString() }]
+      : [],
+    // icons: [{ rel: "icon", url: data.advertiser.store_logo || "/favicon.ico" }],
+    openGraph: {
+      title,
+      description,
+      url: `https://instalanding.shop/${offer_id}`,
+      images: [
+        {
+          url: imageUrl,
+          width: 1200,
+          height: 630,
+          alt: title,
+        },
+      ],
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [imageUrl],
+    },
+    other: {
+      "theme-color": data?.config?.primary_color || "#FFFFFF",
+      "twitter:image": imageUrl,
+      "twitter:card": "summary_large_image",
+      "og:url": `https://instalanding.shop/${offer_id}`,
+      "og:image": imageUrl,
+      "og:type": "website",
+    },
+  };
+}
