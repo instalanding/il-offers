8b8964972c6c3fc7c6e777511fb7381ffe25f3f7
yash-instalanding <yash.verma@instalanding.in>
2025-03-18 15:45:31 +0530

    Apply changes from commit 7d34c4b2e9cc26f0fbef5b119dffb4594ae40f86: Refactor checkout components

--- components/offers/components/Footer.tsx
+++ components/offers/components/Footer.tsx
@@ -1,24 +1,17 @@
-import React, { useState } from "react";
-import { calculatePercentageOff } from "@/lib/calculateDiscount";
-import { formatPrice } from "@/lib/formatUtils";
-// import useCheckout from "@/hooks/Checkout";
+import React from "react";
 import FingerprintJS from "@fingerprintjs/fingerprintjs";
 import axios from "axios";
-import { useRouter } from "next/navigation";
-import { Button } from "@/components/ui/button";
-import { IoIosAdd, IoIosRemove } from "react-icons/io";
-// import { TbShoppingBagPlus } from "react-icons/tb";
-import { isMobile } from "react-device-detect";
-import { FaSpinner } from "react-icons/fa";
+import FastrrCheckout from "./Checkouts/FastrrCheckout";
+import ShopifyCheckout from "./Checkouts/ShopifyCheckout";
 
-interface Config {
+export interface Config {
   primaryColor: string;
   secondaryColor: string;
   footerText: string;
   buttonText: string;
 }
 
-interface Price {
+export interface Price {
   offerPrice: {
     prefix: string;
     value: string;
@@ -30,7 +23,7 @@ interface Price {
   quantity: boolean;
 }
 
-interface Checkout {
+export interface Checkout {
   campaign_id: string;
   variant_id: string;
   offer_id: string;
@@ -59,9 +52,6 @@ const Footer: React.FC<{
   handleDecrease,
   checkoutData,
 }) => {
-    // const { handleCheckout, handleMouseEnter, handleTouchStart } = useCheckout();
-    const [loading, setLoading] = useState<boolean>(false);
-    const router = useRouter();
 
     const getVisitorId = async () => {
       if (typeof window === "undefined") return;
@@ -75,88 +65,6 @@ const Footer: React.FC<{
         return null;
       }
     };
-
-    const handleCheckoutButtonClick = async () => {
-      if (checkoutData.checkout_name === "shopify") {
-        router.push(
-          `https://${checkoutData.store_url}/cart/${checkoutData.variant_id}:${quantity}?discount=${checkoutData.coupon_code}`
-        );
-      } else {
-        router.push(
-          `https://${checkoutData.store_url}/cart/${checkoutData.variant_id}:${quantity}?discount=${checkoutData.coupon_code}`
-        );
-      }
-      recordClicks();
-      if (checkoutData.pixel && Array.isArray(checkoutData.pixel)) {
-        checkoutData.pixel.forEach((pixelId) => {
-          const noscript = document.createElement("noscript");
-          const img = document.createElement("img");
-          img.height = 1;
-          img.width = 1;
-          img.style.display = "none";
-          img.src = `https://www.facebook.com/tr?id=${pixelId}&ev=Checkout&noscript=1`;
-          img.alt = "Facebook Pixel";
-          noscript.appendChild(img);
-          document.body.appendChild(noscript);
-        });
-      }
-    }
-
-    // const handleCheckoutButtonClick = async (
-    //   e: React.MouseEvent<HTMLButtonElement>
-    // ) => {
-    //   // setLoading(true);
-    //   try {
-    //     if (isMobile) {
-    //       handleTouchStart();
-    //     }
-    //     if (
-    //       checkoutData.checkout_name === "shiprocket" ||
-    //       checkoutData.checkout_name === "fastr" ||
-    //       checkoutData.checkout_name === "fastrr"
-    //     ) {
-    //       setTimeout(() => {
-    //         //console.log("checkout called");
-    //         handleCheckout(
-    //           e as React.MouseEvent<HTMLButtonElement, MouseEvent>,
-    //           checkoutData.variant_id,
-    //           checkoutData.offer_id,
-    //           checkoutData.coupon_code,
-    //           checkoutData.utm_params,
-    //           quantity
-    //         );
-    //       }, 3000);
-    //     } else if (checkoutData.checkout_name === "shopify") {
-    //       router.push(
-    //         `https://${checkoutData.store_url}/cart/${checkoutData.variant_id}:${quantity}?discount=${checkoutData.coupon_code}`
-    //       );
-    //     } else {
-    //       router.push(
-    //         `https://${checkoutData.store_url}/cart/${checkoutData.variant_id}:${quantity}?discount=${checkoutData.coupon_code}`
-    //       );
-    //     }
-    //     recordClicks();
-    //     if (checkoutData.pixel && Array.isArray(checkoutData.pixel)) {
-    //       checkoutData.pixel.forEach((pixelId) => {
-    //         const noscript = document.createElement("noscript");
-    //         const img = document.createElement("img");
-    //         img.height = 1;
-    //         img.width = 1;
-    //         img.style.display = "none";
-    //         img.src = `https://www.facebook.com/tr?id=${pixelId}&ev=Checkout&noscript=1`;
-    //         img.alt = "Facebook Pixel";
-    //         noscript.appendChild(img);
-    //         document.body.appendChild(noscript);
-    //       });
-    //     }
-    //   } catch (error) {
-    //     console.error("Error during checkout:", error);
-    //   } finally {
-    //     setTimeout(() => {
-    //       setLoading(false);
-    //     }, 6000);
-    //   }
-    // };
 
     async function recordClicks() {
       try {
@@ -176,153 +84,33 @@ const Footer: React.FC<{
       ? Math.round(parseFloat(price.offerPrice.value))
       : 0;
 
-    //console.log(loading);
 
     return (
-      <>
-        <input type="hidden" value={checkoutData.store_url} id="sellerDomain" />
-        <div
-          className="sticky bottom-0 bg-gray-100"
-        >
-          <div className="flex flex-col">
-            <div
-              style={{
-                backgroundColor: config?.primaryColor + "9a",
-                color: config.secondaryColor || "#000000",
-              }}
-              className="p-[1px] shadow-md flex justify-center items-center gap-2"
-            >
-              <p className="text-sm text-center">{config.footerText}</p>
-            </div>
-
-            {/* Price and Button Section */}
-            <div className="flex items-center justify-between text-black p-[10px] rounded-lg gap-2 w-full">
-              <div className="flex items-center justify-center shrink-0 w-auto">
-                {price?.offerPrice?.value ? (
-                  <div className="flex flex-col gap-1 px-2">
-                    {price?.originalPrice?.value &&
-                      parseFloat(price.offerPrice.value) <
-                      parseFloat(price.originalPrice.value) ? (
-                      <div className="flex flex-col justify-center items-start">
-                        {/* Offer Price on top */}
-                        <p
-                          style={{ color: config?.primaryColor }}
-                          className="font-bold text-[20px] text-center"
-                        >
-                          {formatPrice(
-                            roundedOfferPrice,
-                            price.offerPrice.prefix
-                          )}
-                        </p>
-                        {/* Original Price and Discount in same row below */}
-                        <div className="flex items-center gap-2">
-                          <p className="text-[11px] text-gray-600 line-through">
-                            {formatPrice(
-                              parseFloat(price.originalPrice.value),
-                              price.originalPrice.prefix
-                            )}
-                          </p>
-                          <p className="text-[11px] text-red-600">
-                            {calculatePercentageOff(
-                              parseFloat(price.originalPrice.value),
-                              parseFloat(price.offerPrice.value)
-                            )}
-                            % off
-                          </p>
-                        </div>
-                      </div>
-                    ) : (
-                      <p
-                        style={{ color: config?.primaryColor }}
-                        className="text-[20px] font-bold"
-                      >
-                        {formatPrice(
-                          roundedOfferPrice,
-                          price.offerPrice.prefix
-                        )}
-                      </p>
-                    )}
-                  </div>
-                ) : price?.originalPrice?.value ? (
-                  <p className="text-[15px] font-semibold text-green-600">
-                    {formatPrice(
-                      parseFloat(price.originalPrice.value),
-                      price.originalPrice.prefix
-                    )}
-                  </p>
-                ) : (
-                  ""
-                )}
-              </div>
-              <div className="flex gap-2">
-                {/* Quantity Selector */}
-                {price.quantity === true && (
-                  <div className="flex items-center gap-2 border rounded-lg py-2 px-1 w-auto">
-                    <button
-                      onClick={handleDecrease}
-                      disabled={quantity === 1}
-                      className="disabled:opacity-50"
-                    >
-                      <IoIosRemove size={18} />
-                    </button>
-                    <span className="text-lg font-semibold">{quantity}</span>
-                    <button onClick={handleIncrease}>
-                      <IoIosAdd size={18} />
-                    </button>
-                  </div>
-                )}
-                {/* Removing the shiprocket and fastrr checkout buttons */}
-                <Button
-                  onClick={(e) => {
-                    // setLoading(true);
-                    handleCheckoutButtonClick();
-                  }}
-                  disabled={isSoldOut}
-                  className={`max-w-[300px] border flex items-center justify-center text-[18px] gap-2 px-8 py-2 h-full flex-1 rounded-lg transition-colors
-                ${isSoldOut ? "opacity-50 cursor-not-allowed" : ""}`}
-                  style={{
-                    backgroundColor: config.primaryColor,
-                    color: config.secondaryColor,
-                  }}
-                >
-                  {isSoldOut ? "Sold Out" : <>{config.buttonText}</>}
-                </Button>
-                {/* {loading ? (
-                  <Button
-                    className={`max-w-[300px] border flex items-center justify-center text-[18px] gap-2 px-8 py-2 h-full flex-1 rounded-lg`}
-                    disabled={loading}
-                    style={{
-                      backgroundColor: config.primaryColor,
-                      color: config.secondaryColor,
-                    }}
-                  >
-                    <FaSpinner className="animate-spin" /> Processing...
-                  </Button>
-                ) : (
-                  <Button
-                    onClick={(e) => {
-                      setLoading(true);
-                      handleCheckoutButtonClick(e);
-                    }}
-                    onMouseEnter={handleMouseEnter}
-                    onTouchStart={handleTouchStart}
-                    disabled={isSoldOut}
-                    className={`max-w-[300px] border flex items-center justify-center text-[18px] gap-2 px-8 py-2 h-full flex-1 rounded-lg transition-colors
-                ${isSoldOut ? "opacity-50 cursor-not-allowed" : ""}`}
-                    style={{
-                      backgroundColor: config.primaryColor,
-                      color: config.secondaryColor,
-                    }}
-                  >
-                    {isSoldOut ? "Sold Out" : <>{config.buttonText}</>}
-                  </Button>
-                )} */}
-              </div>
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  };
+      checkoutData.checkout_name === "shiprocket" ||
+        checkoutData.checkout_name === "fastr" ||
+        checkoutData.checkout_name === "fastrr" ?
+        <FastrrCheckout
+          checkoutData={checkoutData}
+          config={config}
+          price={price}
+          quantity={quantity}
+          handleIncrease={handleIncrease}
+          handleDecrease={handleDecrease}
+          roundedOfferPrice={roundedOfferPrice}
+          isSoldOut={isSoldOut}
+          recordClicks={recordClicks}
+        /> : <ShopifyCheckout
+          checkoutData={checkoutData}
+          config={config}
+          price={price}
+          quantity={quantity}
+          handleIncrease={handleIncrease}
+          handleDecrease={handleDecrease}
+          roundedOfferPrice={roundedOfferPrice}
+          isSoldOut={isSoldOut}
+          recordClicks={recordClicks}
+        />
+    )
+}
 
 export default Footer;
