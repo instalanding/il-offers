a3850614657286dc2a7fb7cb0640f217139f757e
yash-instalanding <yash.verma@instalanding.in>
2025-03-20 23:18:35 +0530

    add new changes

--- components/offers/components/Reviews/Stack.tsx
+++ components/offers/components/Reviews/Stack.tsx
@@ -31,8 +31,6 @@ function processRating(rating: number): { fullStars: number; hasHalfStar: boolea
 
 const Stack: React.FC<ReviewStackProps> = ({ reviews, totalStars, Star }) => {
 
-  console.log(reviews, "reviews");
-
   return (
     <div className="flex flex-col gap-2">
       {reviews.map((review, index) => {
