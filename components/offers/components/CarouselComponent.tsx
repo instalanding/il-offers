ef08b41e811b7aaf7f50a061908ee5354d47acb8
Chhavi <chhavi.gupta@instalanding.in>
2025-04-03 20:03:46 +0530

    carousel fixes

--- components/offers/components/CarouselComponent.tsx
+++ components/offers/components/CarouselComponent.tsx
@@ -48,31 +48,46 @@ const CarouselComponent: React.FC<CarouselProps> = ({ images, variantId }) => {
     }, [showBadge, badgeUrl]);
 
     // Memoize rendering of the carousel items to prevent recreating on each render
-    const carouselItems = useMemo(() => (
-        // finalImages.map((image, index) => (
-            // <CarouselItem key={`carousel-item-${index}-${image.url}`}>
+    const carouselItems = useMemo(() =>
+        finalImages.map((image, index) => (
+            <CarouselItem key={`carousel-item-${index}-${image.url}`}>
                 <div className="relative">
                     <ProductImage
-                        src={finalImages[0]?.url}
-                        alt={"Main Product Image"}
+                        src={image?.url}
+                        alt={index === 0 ? "Main Product Image" : `Product Image ${index + 1}`}
                         width={480}
                         height={480}
-                        className={'main-product-image'}
+                        className={`w-full ${index === 0 ? 'main-product-image' : ''}`}
+                        isLCP={index === 0} // Mark the first image as LCP
                     />
+
+                    {showBadge && index === 0 && (
+                        <div className="absolute top-2 left-1 z-10">
+                            <img
+                                ref={badgeRef}
+                                src={badgeUrl}
+                                alt="Badge"
+                                width={60}
+                                height={60}
+                                className="w-[60px] h-[60px]"
+                            />
+                        </div>
+                    )}
                 </div>
-            // </CarouselItem>
-        // )),
-    ), [finalImages]);
+            </CarouselItem>
+        )),
+        [finalImages, showBadge, badgeUrl]
+    );
 
     return (
         <div className="carousel-wrapper">
-            {/* <Carousel opts={{ startIndex: initialSlide }}>
-                <CarouselContent> */}
+            <Carousel opts={{ startIndex: initialSlide }}>
+                <CarouselContent>
                     {carouselItems}
-                {/* </CarouselContent>
+                </CarouselContent>
                 <CarouselPrevious className="left-[7px] shadow-md" />
                 <CarouselNext className="right-[7px] shadow-md" />
-            </Carousel> */}
+            </Carousel>
         </div>
     );
 };
